f0a9d8e742b38439e1533aeaba10aeb7f952aa9c
Alexander <AlexOpryshko@yandex.ru>
2014-09-25 12:56:56 +0400

    add response handler

--- http/response_header.cpp
+++ http/response_header.cpp
@@ -1,42 +1,87 @@
 #include "response_header.h"
 
+std::map<int, std::string> response_status_code = {
+        {200, "OK"},
+        {403, "Forbidden"},
+        {404, "Not Found"},
+        {405, "Method Not Allowed"}
+};
+std::map<std::string, std::string> response_content = {
+        {"html", "text/html"},
+        {"css", "text/css"},
+        {"js", "text/javascript"},
+        {"jpg", "image/jpeg"},
+        {"jpeg", "image/jpeg"},
+        {"png", "image/png"},
+        {"gif", "image/gif"},
+        {"swf", "application/x-shockwave-flash"},
+        {"unknown", "application/octet-stream"}
+};
+
+std::regex extension_regex("\\.([^.]+)$");
+
 response_header::response_header() {
-    httpVersion = "HTTP/1.1";
+    http_version = "HTTP/1.1";
     date = time(0);
     server = "tech-mail.ru http-server (via alexopryshko)";
 }
 
-void response_header::setContentLength(int contentLength) {
-    this->contentLength = contentLength;
+void response_header::set_content_length(long _content_length) {
+    content_length = _content_length;
 }
 
-void response_header::setContentType(std::string contentType) {
-    this->contentType = contentType;
+bool response_header::set_content_type_from_path(std::string path) {
+    std::smatch extension;
+    if (std::regex_search(path, extension, extension_regex) && extension.size() > 0) {
+        try {
+            content_type = response_content.at(extension[1]);
+        }
+        catch (...) {
+            content_type = "application/octet-stream";
+        }
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
-void response_header::setStatusCode(int statusCode, std::string reasonPhrase) {
-    this->statusCode = statusCode;
-    this->reasonPhrase = reasonPhrase;
+void response_header::set_content_type(std::string _content_type) {
+    try {
+        content_type = response_content.at(_content_type);
+    }
+    catch (...) {
+        content_type = "application/octet-stream";
+    }
 }
 
+void response_header::set_status_code(int _status_code) {
+    try {
+        reason_phrase = response_status_code.at(_status_code);
+        status_code = _status_code;
+    }
+    catch (...) {
+        _status_code = 500;
+    }
+}
 std::string response_header::getHeader() {
     std::string buffer = "";
-    buffer += httpVersion;
+    buffer += http_version;
     buffer += SplitChars::SP;
-    buffer += std::to_string(statusCode);
+    buffer += std::to_string(status_code);
     buffer += SplitChars::SP;
-    buffer += reasonPhrase;
-
+    buffer += reason_phrase;
     buffer += SplitChars::LF;
     buffer += "Connection: close";
     buffer += SplitChars::LF;
     std::string strData(ctime(&date));
     buffer += "Date: " + strData;
-    buffer += SplitChars::LF;
     buffer += "Server: " + server;
     buffer += SplitChars::LF;
-    buffer += "Content-Length: " + std::to_string(contentLength);
-    buffer += "Content-Type: " + contentType + "; " + "charset='utf-8'";
+    buffer += "Content-Length: " + std::to_string(content_length);
+    buffer += SplitChars::LF;
+    buffer += "Content-Type: " + content_type + "; " + "charset='utf-8'";
     buffer += SplitChars::CRLF + SplitChars::CRLF;
+
     return buffer;
 }
--- http/response_header.h
+++ http/response_header.h
@@ -1,28 +1,30 @@
 #include <iostream>
-#include "utilities.h"
+#include "request_header.h"
 #include "constants.h"
+#include <map>
 
 class response_header {
 public:
     response_header();
-    ~response_header() {};
 
-    void setStatusCode(int statusCode, std::string reasonPhrase);
-    void setContentType(std::string contentType);
-    void setContentLength(int contentLength);
-    int getContentLength() { return contentLength; }
+    void set_status_code(int _status_code);
+    bool set_content_type_from_path(std::string path);
+    void set_content_type(std::string _content_type);
+    void set_content_length(long _content_length);
+    long get_content_length() { return content_length; }
+
     std::string getHeader();
 
 private:
     //status-line
-    std::string httpVersion;
-    int statusCode;
-    std::string reasonPhrase;
+    std::string http_version;
+    int status_code;
+    std::string reason_phrase;
     //general-header
     std::time_t date;
     //response-header
     std::string server;
     //entity-header
-    int contentLength;
-    std::string contentType;
+    long content_length;
+    std::string content_type;
 };
